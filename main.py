07d062c9d8474c6abfb93c32573d8a209f10f9d4
killerarz <milov52@gmail.com>
2023-05-14 17:47:41 +0300

    fixed after review

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import asyncio
 import curses
 import random
 import time
+from itertools import cycle
 
 from curses_tools import draw_frame, fire, get_frame_size, read_controls
 
 TIC_TIMEOUT = 0.1
 STAR_COUNTS = 100
+BORDER_WIDTH = 1
+ANIMATE_FRAME_PATH = 'figures'
 
 
 async def go_to_sleep(seconds):
@@ -28,58 +31,64 @@ async def blink(canvas, row, column, offset_tics, symbol='*'):
         await go_to_sleep(.3)
 
 
-async def animate_spaceship(canvas, row, column, frame1, frame2):
+async def animate_spaceship(canvas, row, column, frame_size, animation_frames):
     canvas.nodelay(True)
-    frame_row, frame_column = get_frame_size(frame1)
-    max_y, max_x = curses.window.getmaxyx(canvas)
 
-    while True:
+    canvas_height, canvas_width = curses.window.getmaxyx(canvas)
+    frame_width, frame_height = frame_size
+
+    for frame in animation_frames:
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
         row += rows_direction
         column += columns_direction
 
-        row = max(0, row) and min(row, max_y - frame_row)
-        column = max(0, column) and min(column, max_x - frame_column)
+        if row < BORDER_WIDTH or column < BORDER_WIDTH:
+            row = max(BORDER_WIDTH, row)
+            column = max(BORDER_WIDTH, column)
+        else:
+            row = min(row, canvas_height - BORDER_WIDTH - frame_height)
+            column = min(column, canvas_width - BORDER_WIDTH - frame_width)
 
-        draw_frame(canvas, row, column, frame1),
-        canvas.refresh()
+        draw_frame(canvas, row, column, frame)
         await go_to_sleep(.1)
-        draw_frame(canvas, row, column, frame1, negative=True),
-
-        draw_frame(canvas, row, column, frame2),
-        canvas.refresh()
-        await go_to_sleep(.1)
-        draw_frame(canvas, row, column, frame2, negative=True)
+        draw_frame(canvas, row, column, frame, negative=True)
 
 
-def draw_spaceship(canvas, max_x, max_y):
-    with open("figures/rocket_frame_1.txt", "r") as rocket1_frame, \
-            open("figures/rocket_frame_2.txt", "r") as rocket2_frame:
+def draw_spaceship(canvas, canvas_width, canvas_height):
+    with open(f"{ANIMATE_FRAME_PATH}/rocket_frame_1.txt", "r") as rocket1_frame, \
+            open(f"{ANIMATE_FRAME_PATH}/rocket_frame_2.txt", "r") as rocket2_frame:
         frame1 = rocket1_frame.read()
         frame2 = rocket2_frame.read()
 
-    return animate_spaceship(canvas, max_y // 2, max_x // 2 - 2, frame1, frame2)
+    frame_height, frame_width = get_frame_size(frame1)
+
+    row, column = canvas_height // 2, canvas_width // 2 - frame_width // 2
+
+    animation_frames = cycle([frame1, frame2])
+    return animate_spaceship(canvas, row, column, (frame_width, frame_height), animation_frames)
 
 
 def draw(canvas):
     curses.curs_set(False)
-    max_y, max_x = curses.window.getmaxyx(canvas)
+    canvas_height, canvas_width = curses.window.getmaxyx(canvas)
 
-    coroutine_spaceship = draw_spaceship(canvas, max_x, max_y)
-    coroutine_fire = fire(canvas, max_y // 2, max_x // 2)
+    coroutine_spaceship = draw_spaceship(canvas, canvas_width - 1, canvas_height - 1)
+    coroutine_fire = fire(canvas, canvas_height // 2, canvas_width // 2)
 
     coroutines = []
     for _ in range(STAR_COUNTS):
-        row, column = (random.randint(0, max_y - 1), random.randint(0, max_x - 1))
+        row, column = (random.randint(0, canvas_height - BORDER_WIDTH),
+                       random.randint(0, canvas_width - BORDER_WIDTH))
+
         symbol = random.choice('+*.:')
         offset_tics = random.randint(0, 3)
         coroutines.append(blink(canvas, row, column, offset_tics, symbol))
 
     coroutines.append(coroutine_fire)
     coroutines.append(coroutine_spaceship)
+    canvas.border()
     while True:
         try:
-            canvas.border()
             for coroutine in coroutines.copy():
                 coroutine.send(None)
 
@@ -87,8 +96,6 @@ def draw(canvas):
             time.sleep(TIC_TIMEOUT)
         except StopIteration:
             coroutines.remove(coroutine)
-        if len(coroutines) == 0:
-            break
 
 
 if __name__ == '__main__':
